181be0e4982a689f21fe99c1bf676749e6f8ef16
bertolo1988 <tiagobertolo@gmail.com>
2016-08-31 15:17:26 +0100

    added routes and bigger interval to config file

--- config.js
+++ config.js
@@ -1,13 +1,37 @@
 module.exports = {
 	flightScrappper: {
-		periods: 2,
-		interval: 72,
+		periods: 60,
+		interval: 96,
 		routes: [{
 			from: 'LIS',
 			to: 'LON'
 		}, {
 			from: 'LIS',
 			to: 'MIL'
+		}, {
+			from: 'LIS',
+			to: 'PAR'
+		}, {
+			from: 'LIS',
+			to: 'BCN'
+		}, {
+			from: 'LIS',
+			to: 'IST'
+		}, {
+			from: 'LIS',
+			to: 'NYC'
+		}, {
+			from: 'LIS',
+			to: 'BER'
+		}, {
+			from: 'LIS',
+			to: 'RIO'
+		}, {
+			from: 'LIS',
+			to: 'LUA'
+		}, {
+			from: 'LIS',
+			to: 'DXB'
 		}],
 		currency: 'USD',
 		directFlight: 'false',
